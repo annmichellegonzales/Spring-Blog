d4feec8930bcddda0476d2c67decc5f807822781
Ann Michelle Gonzales <ann.michelle.gonzales112@gmail.com>
2021-04-29 10:27:53 -0500

    setting up view

--- src/main/java/com/codeup/SpringBlog/controllers/PostController.java
+++ src/main/java/com/codeup/SpringBlog/controllers/PostController.java
@@ -2,15 +2,32 @@ package com.codeup.SpringBlog.controllers;
 
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class PostController {
 
-    @GetMapping("/posts")
+    @RequestMapping(value = "/posts", method = RequestMethod.GET)
     @ResponseBody
-    public String post() {
-        return "";
+    public String post(String post) {
+        return post;
+    }
+
+    @RequestMapping(value = "/posts/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public String postId(String id) {
+        return id;
+    }
+//
+    @RequestMapping(value = "/posts/create", method = RequestMethod.GET)
+    @ResponseBody
+    public String viewPost(String post) {
+        return post;
+    }
+
+    @PostMapping(value = "/posts/create")
+    @ResponseBody
+    public String createPost(String post) {
+        return post;
     }
 }
