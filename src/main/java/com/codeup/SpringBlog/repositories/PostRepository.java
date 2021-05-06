275c7d909e5e3e74e263d726cd41d5c1323116dc
Ann Michelle Gonzales <ann.michelle.gonzales112@gmail.com>
2021-05-05 19:46:56 -0500

    one to many, etc

--- src/main/java/com/codeup/SpringBlog/repositories/PostRepository.java
+++ src/main/java/com/codeup/SpringBlog/repositories/PostRepository.java
@@ -1,2 +1,9 @@
-package com.codeup.SpringBlog.repositories;public class PostRepository {
+package com.codeup.SpringBlog.repositories;
+
+
+import com.codeup.SpringBlog.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<User, Long> {
 }
+
